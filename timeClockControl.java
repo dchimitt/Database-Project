99ea9d4c92d269abd4a92a30f8f94d3ee077efe0
dchimitt <152079190+dchimitt@users.noreply.github.com>
2024-11-16 18:35:45 -0600

    Add boilerplate .java controller file for timeClockUI scene

---
+++ timeClockControl.java
@@ -0,0 +1,39 @@
+package application;
+
+import javafx.fxml.FXML;
+import javafx.scene.control.Button;
+import javafx.scene.control.ChoiceBox;
+import javafx.scene.control.Label;
+import javafx.scene.control.PasswordField;
+
+public class timeClockControl {
+
+	//FXML Object Linking
+	
+    // Buttons
+    @FXML private Button btnPunch;
+    @FXML private Button btnExit;
+    
+    // Labels
+    @FXML private Label lblTimeIn;
+    @FXML private Label lblTimeInOutput;
+    @FXML private Label lblTimeOutOutput;
+    @FXML private Label lblTimeWorked;
+    @FXML private Label lblTitle;
+    
+    // PasswordField
+    @FXML private PasswordField passwordField; // rename as needed, unsure of function at the moment
+    
+    // ChoiceBox
+    @FXML private ChoiceBox<String> choiceBox; // rename as needed, unsure of function at the moment
+
+    // Handle the action when the Punch button is clicked
+    private void handlePunchButtonAction() {
+    	
+    }
+
+    private void handleExitButtonAction() {
+        System.exit(0);
+    }
+}
+
