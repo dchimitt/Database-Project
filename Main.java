0aa0d41ff1e3849b562a90cae7738a3570b01f05
dchimitt <152079190+dchimitt@users.noreply.github.com>
2024-11-17 11:37:37 -0600

    Add new scenes to top of start method

---
+++ Main.java
@@ -0,0 +1,44 @@
+package application;
+	
+import javafx.application.Application;
+import javafx.stage.Stage;
+import javafx.scene.Scene;
+import javafx.scene.layout.AnchorPane;
+import javafx.scene.layout.BorderPane;
+import javafx.fxml.FXMLLoader;
+
+public class Main extends Application {
+	public static final String URL = "jdbc:mysql://localhost:3306/pointofsale";
+	public static final String USER = "dchimitt";
+	public static final String PASSWORD = "testpassword";
+	
+	@Override
+	public void start(Stage primaryStage) {   	
+		try {
+			AnchorPane backOfficeUI = FXMLLoader.load(getClass().getResource("backOfficeUI.fxml"));
+			AnchorPane countScreen = FXMLLoader.load(getClass().getResource("countScreen.fxml"));
+			AnchorPane drawerManager = FXMLLoader.load(getClass().getResource("drawerManager.fxml"));
+			AnchorPane editPOSScreen = FXMLLoader.load(getClass().getResource("editPOSScreen.fxml"));
+			AnchorPane inventoryScreen = FXMLLoader.load(getClass().getResource("inventoryScreen.fxml"));
+			AnchorPane loginScreen = FXMLLoader.load(getClass().getResource("loginScreen.fxml"));
+			AnchorPane modifyScreen = FXMLLoader.load(getClass().getResource("modifyScreen.fxml"));
+			AnchorPane noteScreen = FXMLLoader.load(getClass().getResource("noteScreen.fxml"));
+			AnchorPane openingScreen = FXMLLoader.load(getClass().getResource("openingScreen.fxml"));
+			AnchorPane recallTicketScreen = FXMLLoader.load(getClass().getResource("recallTicketScreen.fxml"));
+			AnchorPane tenderScreen = FXMLLoader.load(getClass().getResource("tenderScreen.fxml"));
+			AnchorPane timeClockUI = FXMLLoader.load(getClass().getResource("timeClockUI.fxml"));
+			BorderPane userInterface = (BorderPane)FXMLLoader.load(getClass().getResource("userInterface.fxml"));
+			Scene scene = new Scene(timeClockUI, 600, 400);
+			scene.getStylesheets().add(getClass().getResource("application.css").toExternalForm());
+			primaryStage.setScene(scene);
+			primaryStage.setTitle("POS System v1.0");
+			primaryStage.show();
+		} catch(Exception e) {
+			e.printStackTrace();
+		}
+	}
+	
+	public static void main(String[] args) {
+		launch(args);
+	}
+}
