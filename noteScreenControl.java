a2159fc653ad60342eec21b145092832a597c852
dchimitt <152079190+dchimitt@users.noreply.github.com>
2024-11-17 08:37:32 -0600

    Add boilerplate controller file for noteScreen

---
+++ noteScreenControl.java
@@ -0,0 +1,25 @@
+package application;
+
+import javafx.fxml.FXML;
+import javafx.scene.control.Button;
+import javafx.scene.control.TextArea;
+
+public class noteScreenControl {
+
+	// FXML Object Linking
+	
+	// Buttons
+    @FXML private Button btnAddNote;
+    @FXML private Button btnClose;
+    
+    // TextArea
+    @FXML private TextArea noteText;
+
+    private void handleAddNoteButtonClick() {
+    	// Logic to handle action when the Add Note button is clicked
+    }
+
+    private void handleCloseButtonClick() {
+    	// Logic to handle action when the Close button is clicked
+    }
+}
