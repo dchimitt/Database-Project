2133dd0fc169f9d368b07af77e3826ee379c22e6
dchimitt <152079190+dchimitt@users.noreply.github.com>
2024-11-18 19:47:43 -0600

    Fix button indexing to be consistent with 0 indexing

--- modifyScreenControl.java
+++ modifyScreenControl.java
@@ -8,28 +8,28 @@ public class modifyScreenControl {
 	// FXML Object Linking
 	
 	// Row 0 Buttons
+    @FXML private Button btn0_0;
     @FXML private Button btn0_1;
     @FXML private Button btn0_2;
     @FXML private Button btn0_3;
-    @FXML private Button btn0_4;
     
     // Row 1 Buttons
+    @FXML private Button btn1_0;
     @FXML private Button btn1_1;
     @FXML private Button btn1_2;
     @FXML private Button btn1_3;
-    @FXML private Button btn1_4;
     
     // Row 2 Buttons
+    @FXML private Button btn2_0;
     @FXML private Button btn2_1;
     @FXML private Button btn2_2;
     @FXML private Button btn2_3;
-    @FXML private Button btn2_4;
     
     // Row 3 Buttons
+    @FXML private Button btn3_0;
     @FXML private Button btn3_1;
     @FXML private Button btn3_2;
     @FXML private Button btn3_3;
-    @FXML private Button btn3_4;
     
     // Close Button
     @FXML private Button btnClose;
