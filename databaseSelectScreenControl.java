70425eebd454772364a39b9dc63b3f14f3aab765
dchimitt <152079190+dchimitt@users.noreply.github.com>
2024-11-19 19:10:39 -0600

    Add boilerplate controller file for databaseSelectScreen scene

---
+++ databaseSelectScreenControl.java
@@ -0,0 +1,35 @@
+package application; 
+
+import javafx.fxml.FXML;
+import javafx.scene.control.Button;
+
+public class databaseSelectScreenControl { 
+	
+	// FXML Object Linking
+
+	// Buttons
+    @FXML private Button btnEmployeeOnboard;
+    @FXML private Button btnEmployeeDeletion;
+    @FXML private Button btnUpdateEmployee;
+    @FXML private Button btnClose;
+
+    @FXML
+    private void handleOnboardButtonAction() {
+    	// Logic to handle action when the Employee Onboard button is clicked
+    }
+
+    @FXML
+    private void handleDeleteButtonAction() {
+    	// Logic to handle action when the Employee Deletion button is clicked
+    }
+
+    @FXML
+    private void handleUpdateButtonAction() {
+    	// Logic to handle action when the Update Employee button is clicked
+    }
+
+    @FXML
+    private void handleCloseButtonAction() {
+    	// Logic to handle action when the Close button is clicked      
+    }
+}
