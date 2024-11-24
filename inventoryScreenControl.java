b578ae216b0d99d52a2bd6e6846e73be5da75e4a
dchimitt <152079190+dchimitt@users.noreply.github.com>
2024-11-24 02:27:20 -0600

    Implement ingredient list query into dropdown

--- inventoryScreenControl.java
+++ inventoryScreenControl.java
@@ -1,5 +1,13 @@
 package application;
 
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.sql.Statement;
+
+import javafx.collections.FXCollections;
+import javafx.collections.ObservableList;
 import javafx.fxml.FXML;
 import javafx.scene.control.Button;
 import javafx.scene.control.ComboBox;
@@ -25,9 +33,33 @@ public class inventoryScreenControl {
     // ComboBoxes
     @FXML private ComboBox<String> selectIngredientComboBox;
 
-    // Other globals
-    private int inventoryCount;
+    @FXML
+    private void initialize() {
+        // Load list of ingredients into the dropdown list when the application starts
+        loadIngredientList();
+    }
+    
+    // Queries the database and loads full list of employee names into the dropdown list
+    private void loadIngredientList() {
+        ObservableList<String> ingredientNames = FXCollections.observableArrayList();
 
+        try (Connection connection = DriverManager.getConnection(Main.URL, Main.USER, Main.PASSWORD)) {
+            String query = "SELECT ingredientName AS ingredient_name FROM INVENTORY;"; 
+            try (Statement stmt = connection.createStatement();
+                 ResultSet rs = stmt.executeQuery(query)) {
+
+                while (rs.next()) {
+                    String ingredientName = rs.getString("ingredient_name");
+                    ingredientNames.add(ingredientName); 
+                }
+            }
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
+
+        selectIngredientComboBox.setItems(ingredientNames);
+    }
+    
     @FXML
     private void handleAddButtonAction() {
     	// Logic to handle action when the Add button is clicked
